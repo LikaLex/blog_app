218a731ff9f4c265a681e65e20e2e86aea556bc1
Lyubov Onufriyuk <lika.lex555@gmail.com>
2020-07-18 22:07:45 +0300

    Implement post creation

--- Gemfile
+++ Gemfile
@@ -3,8 +3,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'sinatra'
 gem 'sinatra-activerecord'
+gem 'sinatra-contrib'
 gem 'rake'
 gem 'pg'
+gem 'oj'
 group :development do
   gem 'rerun'
+  gem 'pry'
 end
--- blog_app.rb
+++ blog_app.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
 class BlogApp < Sinatra::Base
-  get '/' do
-    "Welcome to BodsadasdasokList! #{User.count}"
+  helpers Sinatra::JSON
+
+  post '/posts' do
+    user = User.find_or_create_by(login: request_payload[:login])
+    post = user.posts.new(
+      author_ip: request_payload[:author_ip],
+      title: request_payload[:title],
+      content: request_payload[:content]
+    )
+    if post.save
+      json post.attributes
+    else
+      status :unprocessable_entity
+      json post.errors.full_messages
+    end
+  end
+
+  def request_payload
+    @request_payload ||= Oj.load(request.body.read).with_indifferent_access
   end
 end
